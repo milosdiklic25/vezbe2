ab94bbd59222321eaaf46a145d62732e2967ae72
Milos Diklic <RA211-2020@ftn.ns>
2022-10-25 15:09:58 +0200

    poruka

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+        public void metoda(int n)
+        {
+            if (n % 10 == 3)
+                Console.WriteLine("Sadrzi 3");
+            else if ((n % 100 > 30) && (n % 100 < 40))
+                Console.WriteLine("Sadrzi 3");
+            else
+                return;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Class1 klasa1 = new Class1();
+            klasa1.metoda(135);
+        }
+    }
+}
